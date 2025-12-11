034e30df417aa8e73624bc84c269cb1a1abd0e5a
Sri Ramya Panja <sriramyapanja123@gmail.com>
2025-12-11 01:17:57 -0500

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT || 3306,
   database: process.env.DB_NAME || 'electronic_health_records',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Srip1898!',
+  password: process.env.DB_PASSWORD || '*****',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
